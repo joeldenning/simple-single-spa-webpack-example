b8d2f7a870daca4b4735f958ebdc2098dcc3376b
Ashish Jangid <ajangi@sapient.com>
2018-02-07 00:54:07 +0530

    changed indentation from 4 to 2

--- src/app1/app1.js
+++ src/app1/app1.js
@@ -4,32 +4,32 @@ import singleSpaReact from 'single-spa-react';
 import Root from './root.component.js';
 
 const reactLifecycles = singleSpaReact({
-    React,
-    ReactDOM,
-    rootComponent: Root,
-    domElementGetter,
+  React,
+  ReactDOM,
+  rootComponent: Root,
+  domElementGetter,
 });
 
 export function bootstrap(props) {
-    return reactLifecycles.bootstrap(props);
+  return reactLifecycles.bootstrap(props);
 }
 
 export function mount(props) {
-    return reactLifecycles.mount(props);
+  return reactLifecycles.mount(props);
 }
 
 export function unmount(props) {
-    return reactLifecycles.unmount(props);
+  return reactLifecycles.unmount(props);
 }
 
 function domElementGetter() {
-    // Make sure there is a div for us to render into
-    let el = document.getElementById('app1');
-    if (!el) {
-        el = document.createElement('div');
-        el.id = 'app1';
-        document.body.appendChild(el);
-    }
+  // Make sure there is a div for us to render into
+  let el = document.getElementById('app1');
+  if (!el) {
+    el = document.createElement('div');
+    el.id = 'app1';
+    document.body.appendChild(el);
+  }
 
-    return el;
+  return el;
 }
